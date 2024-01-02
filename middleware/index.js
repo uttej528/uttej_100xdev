ec2cc1d1e33270ba75669f129bb5f72007601844
uttej528 <96988310+uttej528@users.noreply.github.com>
2024-01-02 22:56:00 +0530

    Add files via upload

---
+++ middleware/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+app.get("/health-checkup", function (req, res) {
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const kidneyId = req.query.kidneyId;
+
+    if (username !== "harkirat" || password !== "pass") {
+        res.status(400).json({ "msg": "Something's up with your inputs" });
+        return;
+    }
+
+    if (kidneyId !== "1" && kidneyId !== "2") {
+        res.status(408).json({ "msg": "Something's up with your inputs" });
+        return;
+    }
+
+    // TODO: Handle the kidneyId logic here
+    res.json({
+        "msg": "your kidney is fine",
+
+    });
+});
+
+
+app.listen(3000);
